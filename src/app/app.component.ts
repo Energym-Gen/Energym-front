2644109ac6344b18ca9a8806557501afa8db9863
Raniel Mendes <raniel.mendes@hotmail.com>
2021-11-03 17:15:44 -0300

    [Feature Rani - Task: Update app.component.ts]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,6 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
-})
-
-@Component({
-  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
