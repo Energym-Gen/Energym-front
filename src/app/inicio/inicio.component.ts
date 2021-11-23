c4c78054b5a0821da067e28e9ca63859934e0452
Matheus <matheus.malmeida7@gmail.com>
2021-11-23 15:39:12 -0300

    Estilizando postagem

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -49,10 +49,10 @@ export class InicioComponent implements OnInit {
   ngOnInit(){
 
     window.scroll(0,0)
-    if(environment.token == ""){
-      alert("Sua sessão expirou, faça o login novamente")
+     if(environment.token == ""){
+       alert("Sua sessão expirou, faça o login novamente")
       this.router.navigate(["/login"])
-    }
+     }
 
     this.authh.refreshToken()
     this.getAllTemas()
